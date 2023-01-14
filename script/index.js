95c818f866cdad2854531a5935ba0cb76b31fb79
Andrey Kordik <andreykordik@yandex.ru>
2023-01-14 09:47:05 +0400

    3 попытка

--- script/index.js
+++ script/index.js
@@ -1,35 +1,40 @@
 let popup = document.querySelector('.popup');
 let popupOpen = document.querySelector('.profile__edit-button');
-let popupClose = document.querySelector('.popup__container-closing-icon');
+let popupClose = document.querySelector('.popup__closing-icon');
 
-popupOpen.addEventListener('click', function () {
-    popup.classList.add('active');
-});
+function popupAppearance () {
+    popup.classList.add('popup_opened');
+};
 
-popupClose.addEventListener('click', function () {
-    popup.classList.remove('active');
-});
+popupOpen.addEventListener('click', popupAppearance); 
+
+
+function popupDisabled() {
+    popup.classList.remove('popup_opened');
+};
+
+
+popupClose.addEventListener('click', popupDisabled); 
+
 
 let formElement = document.querySelector('.popup__container')
-let nameInput = document.querySelector('.popup__container-username')
-let jobInput = document.querySelector('.popup__container-userjob')
-let saveButton = document.querySelector('.popup__container-savebutton')
+let nameInput = document.getElementById('Name')
+let jobInput = document.getElementById ('Job')
 let profileName = document.querySelector('.profile__heading')
 let profileJob = document.querySelector('.profile__subheading')
 
+console.log(nameInput)
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
 }
 
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-saveButton.addEventListener('click', function () {
-    popup.classList.remove('active');
-    
-});
+
 
 
